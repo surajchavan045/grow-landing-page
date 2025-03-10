0680c1d423dc38df9366f5f4c62859c5687b38db
unknown <sc465182@gmail.com>
2025-03-10 17:06:42 +0530

    complete Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './compounts/Header'
+import Hero from './compounts/Hero'
 
 function App() {
 
   return (
     <>
        < Header />
+       < Hero />
     </>
   )
 }
---
+++ src/compounts/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../compounts/Hero.css"
+
+export default function Hero() {
+  return (
+    <main>
+    <p className="main-heading">
+      All things finance, <br /> right here.
+    </p>
+    <p className="main-sub-heading">Built for a growwing India.</p>
+    <a href="/">Get Started</a>
+    <img
+      src="https://assets-netstorage.groww.in/web-assets/billion_groww_desktop/prod/_next/static/media/cityScapeImage1.7be26e20.webp"
+      alt=""
+    />
+  </main>
+  )
+}
